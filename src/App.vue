a0c06a3051a652095eeb64b3f2a9756e63ba3c98
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-05-25 22:20:29 +0200

    feat: axios call

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <script>
+import axios from 'axios';
 import VueMain from './components/VueMain.vue';
 export default {
   name:'App',
+  data () {
+    return {
+      apiBaseUrl: 'http://127.0.0.1:8000/api',
+      apiUrls: {
+        projects: '\projects' 
+      }, 
+      projects: []
+    }
+  },
   components: {
     VueMain
+  },
+  methods: {
+    getProjects() {
+      axios.get(this.apiBaseUrl + this.apiUrls.projects)
+      .then((response) => (
+        console.log(response)
+      ))}
+  },
+  created() {
+    this.getProjects(); 
   }
 }
 </script>

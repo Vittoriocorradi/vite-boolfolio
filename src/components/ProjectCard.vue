4a46f0c6a2fcd6ed54fa5a5542f97400cf067b06
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-05-29 09:38:16 +0200

    feat: route project list

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        project: Object
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <img src="" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <p v-if="project.overview" class="card-text">{{ project.overview.substring(0, this.project.overviewLenght) + '...' }}
+            </p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+</template>
--- src/pages/AppProjects.vue
+++ src/pages/AppProjects.vue
@@ -1,9 +1,44 @@
 <script>
+import axios from 'axios';
+import ProjectCard from '../components/ProjectCard.vue';
 export default {
-    name: 'AppProjects'
+    name: 'AppProjects',
+    components: {
+        ProjectCard
+    },
+    data() {
+        return {
+            apiBaseUrl: 'http://127.0.0.1:8000/api',
+            apiUrls: {
+                projects: '/projects'
+            },
+            projects: []
+        }
+    },
+    methods: {
+        getProjects() {
+            axios.get(this.apiBaseUrl + this.apiUrls.projects)
+                .then((response) => {
+                    console.log(response.data.results);
+                    this.projects = (response.data.results);
+                })
+        }
+    },
+    created() {
+        this.getProjects();
+    }
 }
 </script>
 
 <template>
     <h1>My Projects</h1>
+    <main>
+        <div class="container">
+            <div class="row my-4 gy-4">
+                <div class="col col-md-4" v-for="project in projects">
+                    <ProjectCard :project="project" />
+                </div>
+            </div>
+        </div>
+    </main>
 </template>

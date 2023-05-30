1ee2287e3ba594b7347180db9929d07d5db478f9
Vittoriocorradi <vittoriocorradi2310@gmail.com>
2023-05-30 16:20:10 +0200

    feat: form axios call

--- src/pages/ContactUs.vue
+++ src/pages/ContactUs.vue
@@ -1,13 +1,34 @@
 <script>
+import axios from 'axios';
+import store from '../store';
+
 export default {
     name: 'ContactUs',
     data() {
         return {
-            dataForm: {
+            store,
+            formData: {
                 name: '',
                 email: '',
                 message: ''
             }
+        }
+    },
+    methods: {
+        sendContact() {
+            const data = {
+                name: this.formData.name,
+                email: this.formData.email,
+                message: this.formData.message,
+            };
+
+            axios.post(`${this.store.apiBaseUrl}/contacts`, data)
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
         }
     }
 }
@@ -19,18 +40,18 @@ export default {
             <h1>Contact Us</h1>
             <div class="my-4">Tell us what you think!</div>
             <hr>
-            <form submit.prevent="sendContact">
+            <form @submit.prevent="sendContact">
                 <div class="mb-3">
                     <label for="name" class="form-label">Name</label>
-                    <input type="text" class="form-control" id="name" v-model="dataForm.name">
+                    <input type="text" class="form-control" id="name" v-model="formData.name">
                 </div>
                 <div class="mb-3">
                     <label for="email" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="email" v-model="dataForm.email">
+                    <input type="email" class="form-control" id="email" v-model="formData.email">
                 </div>
                 <div class="mb-3">
                     <label for="message" class="form-label">Example textarea</label>
-                    <textarea class="form-control" id="message" rows="3" v-model="dataForm.message"></textarea>
+                    <textarea class="form-control" id="message" rows="3" v-model="formData.message"></textarea>
                 </div>
                 <button class="btn btn-primary">Send your message</button>
             </form>
